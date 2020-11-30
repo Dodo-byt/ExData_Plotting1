df5488ebcf04b95e0bb5bbe391793b9a21f10217
Dodo-byt <sheetalppardeshi@gmail.com>
2020-11-30 16:52:13 +0530

    exploratory data analysis, exploratory graphs, course project 1

---
+++ Plot1.R
@@ -0,0 +1,19 @@
+##Reading data
+
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+
+##Naming data
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power",
+                  "Voltage","Global_intensity","Sub_metering_1",
+                  "Sub_metering_2","Sub_metering_3")
+
+##subsetting power consumptio data
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+##Plot1 using base plotting
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",
+     main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+##copying to png
+dev.copy(png, file = "Plot1.png")
+dev.off()
